0f2bbcd2d0346c22694299ac274547773acf1bb1
Daniel Klein <daniel.klein@em.ag>
2025-08-06 11:38:12 +0000

    fix: update test client to use test_server_config.json for configuration

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,11 @@ from aas_http_client.client import create_client_by_config, AasHttpClient
 @pytest.fixture(scope="module")
 def cloud_client() -> AasHttpClient:
     try:
-        file = Path("./server_config.json")
+        file = Path("./tests/test_server_config.json").resolve()
+        
+        if not file.exists():
+            raise FileNotFoundError(f"Configuration file {file} does not exist.")
+        
         client = create_client_by_config(file, password="")
     except Exception as e:
         raise RuntimeError("Unable to connect to server.")
