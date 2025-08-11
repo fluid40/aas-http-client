0d1ed1efff6cd1be6e3c14a8840852e12114eb09
Daniel Klein <daniel.klein@em.ag>
2025-08-11 09:30:44 +0000

    feat: implement CI workflow for testing and publishing to PyPI; add new tests for AAS client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,7 @@
 import pytest
 from pathlib import Path
 from aas_http_client.client import create_client_by_config, AasHttpClient
-from basyx.aas.model import AssetAdministrationShell, Submodel
+from basyx.aas.model import AssetAdministrationShell, Submodel, MultiLanguageTextType
 import aas_http_client.utilities.model_builder as model_builder
 import json
 import basyx.aas.adapter.json
@@ -67,7 +67,54 @@ def test_003_post_shells(client: AasHttpClient, shared_aas: AssetAdministrationS
     shells = get_result.get("result", [])
     assert len(shells) == 1
     assert shells[0].get("idShort", "") == shared_aas.id_short
+
+def test_004a_get_shell_by_id(client: AasHttpClient, shared_aas: AssetAdministrationShell):
+    result = client.get_shells_by_id(shared_aas.id)
     
+    assert result is not None
+    assert result.get("idShort", "") == shared_aas.id_short
+
+def test_004b_get_shell_by_id_not_found(client: AasHttpClient):
+    result = client.get_shells_by_id("non_existent_id")
+    
+    assert result is None
+    
+def test_005a_update_shells(client: AasHttpClient, shared_aas: AssetAdministrationShell):
+    # Update the idShort of the shared AAS
+
+    description_text = {"en": "Updated description for unit tests"}
+    shared_aas.description = MultiLanguageTextType(description_text)
+    
+    aas_data_string = json.dumps(shared_aas, cls=basyx.aas.adapter.json.AASToJsonEncoder)
+    aas_data = json.loads(aas_data_string)
+    
+    result = client.put_shells(shared_aas.id, aas_data)
+    
+    assert result
+    
+    get_result = client.get_shells_by_id(shared_aas.id)
+    
+    assert get_result is not None
+    assert get_result.get("description", {})[0].get("text", "") == "Updated description for unit tests"
+    assert get_result.get("idShort", "") == shared_aas.id_short
+    assert get_result.get("id", "") == shared_aas.id
+    assert get_result.get("description", {})[0].get("text", "") == shared_aas.description.get("en", "")
+
+def test_005b_update_shells_with_id(client: AasHttpClient, shared_aas: AssetAdministrationShell):
+    # Update the idShort of the shared AAS
+
+    description_text = {"en": "Updated description for unit tests"}
+    shared_aas.description = MultiLanguageTextType(description_text)
+    
+    aas_data_string = json.dumps(shared_aas, cls=basyx.aas.adapter.json.AASToJsonEncoder)
+    aas_data = json.loads(aas_data_string)
+    
+    aas_data["id"] = "updated_id"  # Ensure the id is included in the update
+    
+    result = client.put_shells(shared_aas.id, aas_data)
+    
+    assert result is False
+
 def test_098_delete_shells(client: AasHttpClient, shared_aas: AssetAdministrationShell):
     result = client.delete_shells_by_id(shared_aas.id)
     
