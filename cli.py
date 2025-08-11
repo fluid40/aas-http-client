ce7dc804c7a26cc0763ad6a888362bb0e1f626ed
Daniel Klein <daniel.klein@em.ag>
2025-08-11 08:53:55 +0000

    fix: update server configuration for consistency; adjust timeouts and base URLs

--- cli.py
+++ cli.py
@@ -1,5 +1,5 @@
-import demo.demo_process as demo_process
-from demo.logging_handler import initialize_logging
+import aas_http_client.demo.demo_process as demo_process
+from aas_http_client.demo.logging_handler import initialize_logging
 
 if __name__ == "__main__":
     initialize_logging()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,13 @@
 import pytest
 from pathlib import Path
 from aas_http_client.client import create_client_by_config, AasHttpClient
+from basyx.aas.model import AssetAdministrationShell, Submodel
+import aas_http_client.utilities.model_builder as model_builder
+import json
+import basyx.aas.adapter.json
 
 @pytest.fixture(scope="module")
-def cloud_client() -> AasHttpClient:
+def client() -> AasHttpClient:
     try:
         file = Path("./tests/test_server_config.json").resolve()
         
@@ -14,8 +18,62 @@ def cloud_client() -> AasHttpClient:
     except Exception as e:
         raise RuntimeError("Unable to connect to server.")
 
+    shells = client.get_shells()
+    if shells is None:
+        raise RuntimeError("No shells found on server. Please check the server configuration.")
+
+    for shell in shells.get("result", []):
+        id = shell.get("id", "")
+        client.delete_shells_by_id(id)
+        
+
     return client
 
-def test_001_connect(cloud_client: AasHttpClient):
-    print("Testing connection to the server...")
-    assert cloud_client is not None
+@pytest.fixture(scope="module")
+def shared_sm() -> Submodel:
+    # create a Submodel
+    return model_builder.create_base_submodel("sm_http_client_unit_tests")
+
+@pytest.fixture(scope="module")
+def shared_aas(client: AasHttpClient, shared_sm: Submodel) -> AssetAdministrationShell:
+    # create an AAS
+    aas = model_builder.create_base_ass("aas_http_client_unit_tests")
+
+    # add Submodel to AAS
+    model_builder.add_submodel_to_aas(aas, shared_sm)
+    
+    return aas
+ 
+def test_001_connect(client: AasHttpClient):
+    assert client is not None
+    
+def test_002_get_shells(client: AasHttpClient, shared_aas: AssetAdministrationShell):
+    result = client.get_shells()
+    assert result is not None
+    shells = result.get("result", [])
+    assert len(shells) == 0
+
+def test_003_post_shells(client: AasHttpClient, shared_aas: AssetAdministrationShell):
+    aas_data_string = json.dumps(shared_aas, cls=basyx.aas.adapter.json.AASToJsonEncoder)
+    aas_data = json.loads(aas_data_string)
+    result = client.post_shells(aas_data)
+    
+    assert result is not None
+    result_id_short = result.get("idShort", "")
+    assert result_id_short == shared_aas.id_short
+    
+    get_result = client.get_shells()
+    assert get_result is not None
+    shells = get_result.get("result", [])
+    assert len(shells) == 1
+    assert shells[0].get("idShort", "") == shared_aas.id_short
+    
+def test_098_delete_shells(client: AasHttpClient, shared_aas: AssetAdministrationShell):
+    result = client.delete_shells_by_id(shared_aas.id)
+    
+    assert result is not None
+    
+    get_result = client.get_shells()
+    assert get_result is not None
+    shells = get_result.get("result", [])
+    assert len(shells) == 0
