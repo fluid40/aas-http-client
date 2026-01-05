278d1ecbcee3cec0fff3b0d20c94cc071d80bc1a
Daniel Klein <daniel.klein@em.ag>
2026-01-05 10:32:47 +0000

    fix: reorder import statements for better organization in __init__.py
    Signed-off-by: Daniel Klein <daniel.klein@em.ag>

--- aas_http_client/__init__.py
+++ aas_http_client/__init__.py
@@ -3,6 +3,11 @@
 import importlib.metadata
 from datetime import datetime, timezone
 
+from aas_http_client.classes.client.aas_client import AasHttpClient, create_client_by_config, create_client_by_dict, create_client_by_url
+from aas_http_client.classes.wrapper.sdk_wrapper import SdkWrapper, create_wrapper_by_config, create_wrapper_by_dict, create_wrapper_by_url
+from aas_http_client.utilities import encoder, model_builder, sdk_tools
+from aas_http_client.utilities.version_check import check_for_update
+
 __copyright__ = f"Copyright (C) {datetime.now(tz=timezone.utc).year} :em engineering methods AG. All rights reserved."
 __author__ = "Daniel Klein"
 
@@ -14,11 +19,6 @@ except importlib.metadata.PackageNotFoundError:
 
 __project__ = "aas-http-client"
 __package__ = "aas-http-client"
-
-from aas_http_client.classes.client.aas_client import AasHttpClient, create_client_by_config, create_client_by_dict, create_client_by_url
-from aas_http_client.classes.wrapper.sdk_wrapper import SdkWrapper, create_wrapper_by_config, create_wrapper_by_dict, create_wrapper_by_url
-from aas_http_client.utilities import encoder, model_builder, sdk_tools
-from aas_http_client.utilities.version_check import check_for_update
 
 check_for_update()
 
